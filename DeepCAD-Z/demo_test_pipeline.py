9784cd2f239490404beeb6d8d4f29bff479e48c4
Yin-Tzu Hsieh <116261795+AllyHsieh@users.noreply.github.com>
2024-07-15 15:17:18 +0800

    Add files via upload

--- DeepCAD-Z/demo_test_pipeline.py
+++ DeepCAD-Z/demo_test_pipeline.py
@@ -1,26 +1,21 @@
 """
-This file will help to demonstrate pipeline for testing microscopy data using the TAG-SPARK algorithm.
-The demo shows how to construct the params and call the relevant functions for testing TAG-SPARK network.
-The demo will automatically download tif file and corresponding model file for demo testing.
+This file will help to demonstrate pipeline for testing microscopy data using the DeepCAD-Z algorithm.
+The demo shows how to construct the params and call the relevant functions for testing DeepCAD-Z network.
 See inside for details.
-"""
-from tagspark.test_collection import testing_class
-from tagspark.utils import get_first_filename
 
+This repository is derived from DeepCAD-RT(https://github.com/cabooster/DeepCAD-RT)
+DeepCAD-Z ver1. 2023  Kai-Chun Jhan
+"""
+from deepcad.test_collection import testing_class
+from deepcad.utils import get_first_filename
 
 # %% Select file(s) to be processed (download if not present)
 
-datasets_path = f'datasets/29_4_2_4'  # folder containing tif files for testing
-denoise_model = f'29_4_2_4_202310260959'  # A folder containing pth models to be tested
+datasets_path = f'datasets/29-4-2-4move2'  # folder containing tif files for testing
+denoise_model = f'29-4-2-4move2_202406221747'  # A folder containing pth models to be tested
 
 # %% First setup some parameters for testing
-test_datasize = 31  # dataset size for training (the number of patches)
 GPU = '0'
-patch_x =  64            # the width and height of 3D patches
-patch_y =  128
-patch_t = 30 
-                       # the time dimension of 3D patches
-overlap_factor = 0                  # the overlap factor between two adjacent patches
 num_workers = 0                       # if you use Windows system, set this to 0.
 
 # %% Setup some parameters for result visualization during testing period (optional)
@@ -28,12 +23,7 @@ num_workers = 0                       # if you use Windows system, set this to 0
 
 test_dict = {
     # dataset dependent parameters
-    'patch_x': patch_x,
-    'patch_y': patch_y,
-    'patch_t': patch_t,
-    'overlap_factor':overlap_factor,
     'scale_factor': 1,                   # the factor for image intensity scaling
-    'test_datasize': test_datasize,
     'datasets_path': datasets_path,
     'pth_dir': './pth',                 # pth file root path
     'denoise_model' : denoise_model,
