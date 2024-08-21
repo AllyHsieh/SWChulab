94363dfb019cfb43e9992cd1027217975d61b129
Yin-Tzu Hsieh <116261795+AllyHsieh@users.noreply.github.com>
2024-08-21 09:23:08 +0800

    Add files via upload

---
+++ DeepCAD-RT/demo_test_pipeline.py
@@ -0,0 +1,74 @@
+"""
+This file will help to demonstrate pipeline for testing microscopy data using the DeepCAD-RT algorithm.
+The demo shows how to construct the params and call the relevant functions for testing DeepCAD-RT network.
+The demo will automatically download tif file and corresponding model file for demo testing.
+See inside for details.
+
+* This demo is also available as a jupyter notebook (see demo_test_pipeline.ipynb) and Colab notebook (see
+DeepCAD_RT_demo_colab.ipynb)
+
+More information can be found in the companion paper.
+"""
+from deepcad.test_collection import testing_class
+from deepcad.movie_display import display
+from deepcad.utils import get_first_filename,download_demo
+
+# %% Select file(s) to be processed (download if not present)
+download_demo_file = False
+if download_demo_file:
+    file_name='29_4_2_4' # select the demo file you want to test (e.g. 'ATP_3D', 'fish_localbrain', 'NP_3D', ...)
+    datasets_path, denoise_model = download_demo(download_filename=file_name)
+else:
+    datasets_path = 'datasets/010824SCNtime v2_4_4_64_128_15%_90s_z320_notimecorr'  # folder containing tif files for testing
+    denoise_model = '010824SCNtime v2_4_4_64_128_15%_90s_z320_notimecorr_202408141806'  # A folder containing pth models to be tested
+
+# %% First setup some parameters for testing
+test_datasize = 1145                # the number of frames to be tested (test all frames if the number exceeds the total number of frames in a .tif file)
+GPU = '0'                             # the index of GPU you will use for computation (e.g. '0', '0,1', '0,1,2')
+patch_x = 128                      # the width and height of 3D patches
+patch_y =128
+patch_t = 1145                        # the time dimension of 3D patches
+overlap_factor = 0                  # the overlap factor between two adjacent patches. 
+                                      # Since the receptive field of 3D-Unet is ~90, seamless stitching requires an overlap (patch_xyt*overlap_factor）of at least 90 pixels.
+num_workers = 0                      # if you use Windows system, set this to 0.
+
+# %% Setup some parameters for result visualization during testing period (optional)
+visualize_images_per_epoch = False  # choose whether to display inference performance after each epoch
+save_test_images_per_epoch = False # choose whether to save inference image after each epoch in pth path
+
+# %% Play the demo noise movie (optional)
+# playing the first noise movie using opencv.
+display_images = False
+
+if display_images:
+    display_filename = get_first_filename(datasets_path)
+    print('\033[1;31mDisplaying the first raw file -----> \033[0m')
+    print(display_filename)
+    display_length = 500  # the frames number of the noise movie
+    # normalize the image and display
+    display(display_filename, display_length=display_length, norm_min_percent=0.5, norm_max_percent=99.8)
+
+test_dict = {
+    # dataset dependent parameters
+    'patch_x': patch_x,
+    'patch_y': patch_y,
+    'patch_t': patch_t,
+    'overlap_factor':overlap_factor,
+    'scale_factor': 1,                   # the factor for image intensity scaling
+    'test_datasize': test_datasize,
+    'datasets_path': datasets_path,
+    'pth_dir': './pth',                 # pth file root path
+    'denoise_model' : denoise_model,
+    'output_dir' : './results',         # result file root path
+    # network related parameters
+    'fmap': 16,                          # the number of feature maps
+    'GPU': GPU,
+    'num_workers': num_workers,
+    'visualize_images_per_epoch': visualize_images_per_epoch,
+    'save_test_images_per_epoch': save_test_images_per_epoch
+}
+# %%% Testing preparation
+# first we create a testing class object with the specified parameters
+tc = testing_class(test_dict)
+# start the testing process
+tc.run()
